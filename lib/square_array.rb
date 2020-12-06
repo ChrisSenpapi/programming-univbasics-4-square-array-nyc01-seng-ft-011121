5042a93ebbebca5a9512a2da34fa42b23cbf1133
Christopher Read <christopherread22@outlook.com>
2020-12-06 23:55:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
-result = Array.new(numbers.size)
+result = Array.new(numbers)
 i = 0 
 
-while i < numbers.size 
+while i < numbers 
 result[i] = numbers[i] ** 2 
 i += 1 
 end
